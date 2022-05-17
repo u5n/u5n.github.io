d40f69864315eaa331ada4067d9df1f738975616
u5n <26353598+u5n@users.noreply.github.com>
2022-05-17 10:56:38 +0800

    .

--- algods/plib/ds/segmentTree/sorted_disjoint_intervals.py
+++ algods/plib/ds/segmentTree/sorted_disjoint_intervals.py
@@ -1,120 +1,124 @@
+"""
+diff with sorted_partition_intervals.py 
+    interval don't contain value, store interval information only
+"""
+
 from sortedcontainers import SortedList
-class Interval:
-    __slots__ = 'l', 'r', 'val'
-    def __init__(self, l, r, val=None):
-        self.l, self.r, self.val = l, r, val
-    def __repr__(s): return f"Interval({s.l},{s.r},{s.val})"
 
 class SortedDisjointIntervals:
     """ des: a collection of ordered and disjoint closed intervals ( endpoint is integers )
     app: the online algorithm of [meeting rooms](https://leetcode.com/problems/meeting-rooms)
     """
     def __init__(self):
-        self.ints = SortedList(key=lambda x: x.l)
+        # map left endpoint to right endpoint
+        # assumption: sortedlist is faster than sorteddict
+        # the code logic insure there won't be duplicates of left_endpoint, there is no need to use SortedDict to avoid duplicate
+        self.itvs = SortedList(key=lambda x:x[0])
 
     def get_sub(self, l, r):
         """ find all intervals in `self` that completely contained in [l,r] 
-        return two indices on `self.ints`, `self.ints[lfind:rfind+1]` are completely contained in [l,r]
-        if no such interval, return the original `lfind` and `rfind`, (`rfind-lfind<0`)
+        return two indices@[lfind, rfind] on `self.itvs`, `self.itvs[lfind:rfind]` are completely contained in [l,r]
+        if no such interval, satisfing `rfind-lfind<=0`
+        assert: l<=r
         """
-        ints = self.ints 
+        itvs = self.itvs 
         # find all intervals that left endpoint in [l,r]
-        lfind = ints.bisect_key_left(l)
-        rfind = ints.bisect_key_right(r) - 1
-        if rfind >= lfind:
-            if ints[rfind].r > r:
-                return lfind, rfind - 1
+        lfind = itvs.bisect_key_left(l)
+        rfind = itvs.bisect_key_right(r)
+        # in case partially overlap
+        if lfind < rfind and itvs[rfind-1][1] > r:
+            rfind -= 1
         return lfind, rfind
 
     def get_overlap(self, l, r):
-        """ [l,r] are indices of intervals on `self` with which the [l,r] overlaps """
-        ints = self.ints 
+        """ [l:r] are indices of intervals on `self` with which the [l,r] overlaps 
+        assert: l<=r
+        """
+        itvs = self.itvs 
         # find all intervals that left endpoint in [l,r]
-        lfind = ints.bisect_key_left(l)
-        rfind = ints.bisect_key_right(r) - 1
-        # add a lefter interval
-        if lfind > 0:
-            if ints[lfind - 1].r >= l:
-                lfind -= 1
+        lfind = itvs.bisect_key_left(l)
+        rfind = itvs.bisect_key_right(r)
+        # in case partially overlap
+        if lfind > 0 and itvs[lfind - 1][1] >= l:
+            lfind -= 1
         return lfind, rfind
 
     def get_super(self, l, r):
-        """ [l,r] are indices of intervals on `self` in which the [l,r] completely contained"""
-        ints = self.ints 
-        lfind = ints.bisect_key_right(l) - 1
-        if ints[lfind].r >= r:
+        """ `lfind` are indices of intervals on `self` in which the [l,r] completely contained
+        assert: l<=r
+        """
+        itvs = self.itvs 
+        lfind = itvs.bisect_key_right(l) - 1
+        if itvs[lfind][1] >= r:
             return lfind
-        return -1
+        return None
 
     def add(self, l, r):
         """ add interval [l,r],
         automatically merge exactly adjacent interval
             eg. interval_union `[1,2],[4,10]`, add `[3,3]` it becomes `[1,10]`
-        then return the reference of the merged interval
+        assert: l<=r
         """
-        ints = self.ints 
-        lfind, rfind = self.get_overlap(l, r)
-        # if a righter interval can merge with [l,r]
-        if rfind < len(ints) - 1 and ints[rfind + 1].l == r + 1:
-            rfind += 1
-
+        itvs = self.itvs 
+        lfind = itvs.bisect_key_left(l)
+        # use `r+1` to contain an interval whose leftendpoint is r+1
+        rfind = itvs.bisect_key_right(r+1) 
         # if a lefter interval can merge with [l,r]
-        if lfind > 0 and ints[lfind - 1].r == l - 1:
+        if lfind > 0 and itvs[lfind - 1][1] >= l - 1:
             lfind -= 1
 
         # remove all overlaping(or exactly adjacent) intervals
-        if lfind <= rfind:
-            l = min(l, ints[lfind].l)
-            r = max(r, ints[rfind].r)
-            for _ in range(rfind - lfind + 1):
-                ints.pop(lfind)
-                # rfind is invalid then
+        if lfind < rfind:
+            l = min(l, itvs[lfind][0])
+            r = max(r, itvs[rfind-1][1])
+            del itvs[lfind: rfind]
 
-        new = Interval(l, r)
-        ints.add(new)
-        return new
+        itvs.add([l, r])
+        return l,r
 
     def remove_point(self, x):
         """ remove_point `x` and break interval into less than 2 intervals at point x """
+        itvs = self.itvs 
         # the rightmost interval with left endpoint <= x
-        ints = self.ints 
-        find = ints.bisect_key_right(x) - 1
+        find = itvs.bisect_key_right(x) - 1
         if find == -1: return
-        tar = ints[find]
-        if tar.r >= x:
-            ints.pop(find)
-            if tar.l != tar.r:
-                if tar.l != x:
-                    ints.add(Interval(tar.l, x - 1))
-                if tar.r != x:
-                    ints.add(Interval(x + 1, tar.r))
+        l, r = itvs[find]
+        if r >= x:
+            itvs.pop(find)
+            if l != r:
+                if l != x:
+                    itvs.add([l, x - 1])
+                if r != x:
+                    itvs.add([x + 1, r])
+
+    def split_at(self, x):
+        """
+        split the interval contain point x into two interval, one of them leftendpoint is x
+        """
+        itvs = self.itvs
+        find = itvs.bisect_key_right(x) - 1
+        if find == -1: return 
+        l, r = itvs[find]
+        if r >= x:
+            if l == x: return 
+            itvs[find] = x-1
+            itvs.add([x, r])
 
     def remove(self, l, r):
         """ remove all intervals completely contained in [l,r], split intervals that partially overlap
+        assert: l<=r
         """
-        ints = self.ints 
-        # find all intervals that left endpoint in [l,r]
-        lfind = ints.bisect_key_left(l)
-        rfind = ints.bisect_key_right(r) - 1
-        # whether ints[rfind] is partially overlap
-        if rfind < len(ints):
-            right = ints[rfind]
-            if right.r > r:
-                right.l = r+1
-                rfind -= 1
+        itvs = self.itvs 
+        # split at two endpoint
+        self.split_at(l)
+        self.split_at(r+1)
+        # now there won't any interval partially overlap with or contain [l,r]
+        lfind = itvs.bisect_key_left(l)
+        rfind = itvs.bisect_key_right(r)
+        if lfind < rfind:
+            del itvs[lfind:rfind]
 
-        # whether ints[lfind] is partially overlap
-        if lfind > 0:
-            ll = ints[lfind-1]
-            if ll.r >= l:
-                ll.r = l-1
-
-        # now ints[lfind:rfind+1] is completely contained in [l:r]
-        if lfind <= rfind:
-            for _ in range(rfind-lfind+1):
-                ints.pop(lfind)
-
-    def __len__(self): return len(self.ints)
+    def __len__(self): return len(self.itvs)
 
 if __name__ == '__main__':
     def test_SortedIntervals_1():
@@ -131,5 +135,5 @@ if __name__ == '__main__':
         tree.add(239, 10000)
         tree.add(24, 30)
         tree.add(24, 3000)
-        assert [(x.l, x.r) for x in tree.ints] == [(-100,22), (24,10000)]
+        assert [(x[0], x[1]) for x in tree.itvs] == [(-100,22), (24,10000)]
     test_SortedIntervals_1()
